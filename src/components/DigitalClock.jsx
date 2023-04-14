92a99acc2422e0be6f482e833b7250db6acc09dd
Cristelos <cristelos@msn.com>
2023-04-14 20:47:29 +0200

    only left the stopwatch

--- src/components/DigitalClock.jsx
+++ src/components/DigitalClock.jsx
@@ -1,3 +1,19 @@
+import { useEffect, useState } from "react"
+
 export default function DigitalClock(){
-    
+
+    const [digital, setDigital] = useState();
+
+    useEffect(() => {
+        setInterval(()=>{
+            const hora = new Date();
+            setDigital(hora.toLocaleTimeString());
+        },1000)
+    }, [])
+
+    return(
+        <div className="reloj-digital">
+            <h1>{digital}</h1>
+        </div>
+    )
 }
